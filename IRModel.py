e2bc11deb82dc54ac83d41a649cb0869b0060ed4
Ariana Daka <ad2104@rit.edu>
2019-03-31 11:47:23 -0400

    Created IR model class
    Signed-off-by: Ariana Daka <ad2104@rit.edu>

---
+++ IRModel.py
@@ -0,0 +1,12 @@
+class IRModel:
+    def __init__(self):
+        pass
+
+    def addDoc(self, doc):
+        pass
+
+    def build(self):
+        pass
+
+    def rankedSearch(self):
+        return []
