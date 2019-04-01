22c8e8234031ff9b1328ed7d0ddc319264b142b7
Nirup Dharshan <ns9805@g.rit.edu>
2019-03-31 22:07:43 -0400

    Cleaned code. Better Chunking regex implemented.
    Signed-off-by: Nirup Dharshan <ns9805@g.rit.edu>

--- notreaddit.py
+++ notreaddit.py
@@ -37,19 +37,3 @@ if __name__ == '__main__':
     for r in results:
         print(articles[r].body)
         articles[r].tree.draw()
-    # tos = []
-    # with open("data.json") as outfile:
-    #     json_data = json.load(outfile)
-    #
-    #     for data in json_data['data']:
-    #         tos.append(
-    #             [x for x in gensim.utils.simple_preprocess(data['content']) if x not in stopwords.words('english')])
-    # print(tos)
-    # model = gensim.models.Word2Vec(
-    #     tos,
-    #     size=len(tos),
-    #     window=10,
-    #     min_count=1,
-    #     workers=10)
-    # model.train(tos, total_examples=len(tos), epochs=10)
-    # print(model.wv.similar_by_word("visit"))
